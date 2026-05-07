46f2af151c733cad176cbb893c64dc65d32168c6
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-05-07 23:56:28 +0530

    global execution contest

---
+++ lect2/app.js
@@ -0,0 +1,18 @@
+//let arr = [1,2,3,true, "string"]
+//for(let a in arr){
+//    console.log(a,"in")   // gives index
+//}
+//for(let a of arr){
+//    console.log(a,"of")   // gives value
+//}
+
+let student = {
+    id:1,
+    name:"bhoomi"
+}
+console.log(student.id)
+console.log(student.name)
+
+var a
+console.log(a)
+a=5

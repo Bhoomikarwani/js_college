55662e4b0fa2907ad5b7c3d98767dc899701466d
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-05-12 23:52:47 +0530

    event listeners

---
+++ lect5/script.js
@@ -0,0 +1,37 @@
+// let name = prompt("enter your name");
+// let h3 = document.querySelector("h3");
+// h3.innerText = "name" + ": " + name;
+
+// let h1 = document.querySelectorAll("h1");
+// console.log(h1[0].innerText);
+
+
+// let input = document.querySelector("input");
+// let h2 = document.querySelector("h2");
+// input.addEventListener("input",function(e){
+//     //console.log("hello");
+//     console.log(e.target.value);
+//     h2.innerText = e.target.value;
+// })
+
+// let h1 = document.querySelector("h1");
+// let p = document.createElement("p");
+// p.innerText="hello";
+// h1.appendChild(p);
+
+
+let input = document.querySelector("input");
+let h2 = document.querySelector("h2");
+let btn = document.querySelector("button");
+
+// btn.addEventListener("click",function(){
+//     let data = input.value;
+//     h2.innerText = data;
+// })
+
+// let arr = [];
+// btn.addEventListener("click",function(){
+//     let data = input.value;
+//     arr.push(data);
+//     h2.innerText = arr;
+// }) 

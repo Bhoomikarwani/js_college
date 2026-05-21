81783220d120e9277fa44d7874ddb1e965a77cf9
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-05-21 15:39:13 +0530

    spread operator , rest parameter , j Query

---
+++ lect10/index.js
@@ -0,0 +1,36 @@
+// let arr = [1,2,3,4,5]
+// let nums = [...arr,5,6]    //spread operator used for concatination
+// console.log(nums);
+
+// let obj ={
+//    id:1,
+//    name:"om"
+// }
+// let data={
+//     ...obj,   // concatinate by spread operator
+//     id:2
+// }
+// console.log(data);
+
+// function sum(a,b,c,...nums){   // rest parameter
+//       console.log(nums);
+// }
+// sum(1,2,3,34,5,6,87,3,2,6);
+
+// if(true){
+//     var a =9;   // let
+// }
+// console.log(a);
+
+
+// let arr =[1,2,3,4,5]
+// let [c,b,a] = arr
+// console.log(c,b,a);
+
+
+// let h1 = document.querySelector("h1")
+// h1.innerText="byee";
+
+$("h1").css("color","red");
+
+
